14aedb184a8a739c540a56f854746b4264856d45
shubham2160 <71221452+shubham2160@users.noreply.github.com>
2021-08-23 17:04:27 +0530

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of char is: "<<sizeof(char)<<"\n";
+    cout<<"size of int is: "<<sizeof(int)<<"\n";
+    cout<<"size of float is: "<<sizeof(float)<<"\n";
+    cout<<"size of double is: "<<sizeof(double)<<"\n";
+
+    return 0;
+}
---
+++ task2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a;
+    float b;
+    cout<<"Please enter your numbers: "<<"\n";
+    cin>>a>>b;
+    cout<<"your ans is: "<<float(a * b)<<"\n";
+
+    return 0;
+}
